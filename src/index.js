86a75bdb62c9f514ee66cd067eff5a592a3f44a5
Vyacheslav-Fialko <vyacheslav1@ukr.net>
2021-08-31 20:22:55 +0300

    feat: refact code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
-module.exports = function solveSudoku(matrix) {
-  let solvedMatrix = matrix;
-
-  if (solved(matrix)) {
-    solvedMatrix = matrix;
-  }
-
-  return solvedMatrix;
-}
-
-function solved(matrix) {
+const solved = (matrix) => {
   const cell = [];
   if (!findEmptySpace(matrix, cell)) {
     return true;
@@ -18,7 +8,11 @@ function solved(matrix) {
   const col = cell[1];
 
   for (let number = 1; number < 10; number += 1) {
-    if (!checkRow(matrix, row, number) && !checkCol(matrix, col, number) && !checkBox(matrix, row - row % 3, col - col % 3, number)) {
+    if (
+      !checkRow(matrix, row, number) &&
+      !checkCol(matrix, col, number) &&
+      !checkBox(matrix, row - (row % 3), col - (col % 3), number)
+    ) {
       matrix[row][col] = number;
       if (solved(matrix)) {
         return true;
@@ -27,9 +21,9 @@ function solved(matrix) {
     }
   }
   return false;
-}
+};
 
-function findEmptySpace(matrix, cell) {
+const findEmptySpace = (matrix, cell) => {
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
       if (matrix[i][j] == 0) {
@@ -40,26 +34,27 @@ function findEmptySpace(matrix, cell) {
     }
   }
   return false;
-}
+};
 
-function checkRow(matrix, row, number) {
+const checkRow = (matrix, row, number) => {
   for (let i = 0; i < 9; i++) {
     if (matrix[row][i] == number) {
       return true;
     }
   }
   return false;
-}
+};
 
-function checkCol(matrix, col, number) {
+const checkCol = (matrix, col, number) => {
   for (let i = 0; i < 9; i++) {
     if (matrix[i][col] == number) {
       return true;
     }
   }
   return false;
-}
-function checkBox(matrix, row, col, number) {
+};
+
+const checkBox = (matrix, row, col, number) => {
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
       if (matrix[i + row][j + col] == number) {
@@ -68,4 +63,14 @@ function checkBox(matrix, row, col, number) {
     }
   }
   return false;
-}
+};
+
+module.exports = function solveSudoku(matrix) {
+  let solvedMatrix = matrix;
+
+  if (solved(matrix)) {
+    solvedMatrix = matrix;
+  }
+
+  return solvedMatrix;
+};
